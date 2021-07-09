dbb748aa1182e6793706e412c7032f5cddb090b8
Kent Rancourt <kent.rancourt@microsoft.com>
2021-07-09 09:49:41 -0400

    wip: more brig term work
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- v2/cli/term/page.go
+++ v2/cli/term/page.go
@@ -5,14 +5,22 @@ import (
 	"github.com/rivo/tview"
 )
 
+// page is a base for composing custom tview.Pages that are compatible with the
+// pageRouter component.
 type page struct {
-	*tview.Flex
-	apiClient core.APIClient
-	router    PageRouter
-	app       *tview.Application
+	*tview.Flex                // page behaves like a layout
+	apiClient   core.APIClient // Used to refresh data
+	router      *pageRouter    // Used for routing to other pages on command
+	app         *tview.Application
 }
 
-func newPage(apiClient core.APIClient, app *tview.Application, router PageRouter) *page {
+// newPage returns a base for composing custom tview.Pages that are compatible
+// with the pageRouter component.
+func newPage(
+	apiClient core.APIClient,
+	app *tview.Application,
+	router *pageRouter,
+) *page {
 	return &page{
 		apiClient: apiClient,
 		router:    router,
